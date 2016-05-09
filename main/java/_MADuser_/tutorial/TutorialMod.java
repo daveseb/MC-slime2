1dad00cb073adf125339dd166ca05f795d8e1b84
daveseb <davesebby@gmail.com>
2016-05-09 10:54:32 -0500

    make separate slimethrow object

--- main/java/_MADuser_/tutorial/TutorialMod.java
+++ main/java/_MADuser_/tutorial/TutorialMod.java
@@ -2,17 +2,20 @@ package _MADuser_.tutorial;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.registry.EntityRegistry;
 import _MADuser_.tutorial.init.TutorialItems;
-import _MADuser_.tutorial.items.EntitySlimeball;
 import _MADuser_.tutorial.proxy.CommonProxy;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class TutorialMod {
+
+	@Instance(Reference.MOD_ID)
+	public static TutorialMod instance;
+	    
 
 	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
 	public static CommonProxy proxy;
@@ -20,8 +23,8 @@ public class TutorialMod {
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event){
 		TutorialItems.init();
+
 		TutorialItems.register();
-		EntityRegistry.registerModEntity(EntitySlimeball.class, "tm:slimeball_entity", 1, this, 80, 3, true);
 	}
 	
 	@EventHandler
--- main/java/_MADuser_/tutorial/init/TutorialItems.java
+++ main/java/_MADuser_/tutorial/init/TutorialItems.java
@@ -2,35 +2,45 @@ package _MADuser_.tutorial.init;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.entity.RenderSnowball;
+import net.minecraft.entity.projectile.EntitySnowball;
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import _MADuser_.tutorial.Reference;
+import _MADuser_.tutorial.TutorialMod;
+import _MADuser_.tutorial.items.EntitySlimeball;
 import _MADuser_.tutorial.items.ItemSlimeball;
 
 public class TutorialItems {
 	
 	public static Item test_item;
 	public static Item slimeball_item;
-	public static Item Slimeball_entity;
+	public static Item slimethrow;
 	
 	public static void init(){
 		test_item = new Item().setUnlocalizedName("test_item");
 		slimeball_item = new ItemSlimeball().setUnlocalizedName("slimeball_item");
-		//Slimeball_entity = new EntitySlimeball().setUnlocalizedName("Slimeball_entity");
+		slimethrow = new Item().setUnlocalizedName("slimethrow");
 	}
 	
 	public static void register(){
 		GameRegistry.registerItem(test_item, test_item.getUnlocalizedName().substring(5));
 		GameRegistry.registerItem(slimeball_item, slimeball_item.getUnlocalizedName().substring(5));
-		//GameRegistry.registerItem(Slimeball_entity, Slimeball_entity.getUnlocalizedName().substring(5));
+		GameRegistry.registerItem(slimethrow, slimethrow.getUnlocalizedName().substring(5));
+		
+		EntityRegistry.registerModEntity(EntitySlimeball.class, "slimethrow", 1, TutorialMod.instance, 80, 3, true);
 	}
 	
 	public static void registerRenders(){
 		registerRender(test_item);
 		registerRender(slimeball_item);
-		//registerRender(Slimeball_entity);
+		registerRender(slimethrow);
+		
+		RenderingRegistry.registerEntityRenderingHandler(EntitySlimeball.class, new RenderSnowball<EntitySnowball>(Minecraft.getMinecraft().getRenderManager(), slimethrow, Minecraft.getMinecraft().getRenderItem()));
 	}
+
 	
 	public static void registerRender(Item item){
 		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0, new ModelResourceLocation(Reference.MOD_ID + ":" + item.getUnlocalizedName().substring(5), "inventory"));
